a4d6ad5e14b3d2f7d890593771e0a9bfa1860f72
hithesh111 <hithesh111@gmail.com>
2022-02-15 18:12:25 +0530

    Added two new features Volatility and Percent Change

--- Experimentation/prepare_data.py
+++ Experimentation/prepare_data.py
@@ -5,6 +5,8 @@ import joblib
 caplin_point = yf.download("CAPLIPOINT.NS", start = "2015-01-01", end = "2022-02-03")
 
 caplin_point["Close-Open"] = caplin_point["Close"] - caplin_point["Open"]
+caplin_point["Percent_Change"] = 100*caplin_point["Close-Open"]/caplin_point["Open"]
+caplin_point["Volatility"] = 100*(caplin_point["High"] - caplin_point["Low"])/caplin_point["Open"] 
 caplin_point["Target"] = caplin_point["Close-Open"].apply(lambda x: 1 if x>0 else 0).shift(-1)
 
 joblib.dump(caplin_point, "CAPLIN_POINT.joblib")
