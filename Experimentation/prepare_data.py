ee81347e879da81feae1fd064085e0b9aa2ec35c
hithesh111 <hithesh111@gmail.com>
2022-02-15 18:03:03 +0530

    Used Pandas Shift Function and Added Feature
    Added Close - Open as a Feature

--- Experimentation/prepare_data.py
+++ Experimentation/prepare_data.py
@@ -2,10 +2,9 @@ from cmath import nan
 import yfinance as yf
 import joblib
 
-caplin_point = yf.download("CAPLIPOINT.NS", start = "2020-01-01", end = "2022-02-03")
+caplin_point = yf.download("CAPLIPOINT.NS", start = "2015-01-01", end = "2022-02-03")
 
-y = list((caplin_point["Close"] - caplin_point["Open"]).apply(lambda x: 1 if x>0 else 0))
-y_shifted = [float("nan")] + y[:-1]
-caplin_point["Target"] = y_shifted
+caplin_point["Close-Open"] = caplin_point["Close"] - caplin_point["Open"]
+caplin_point["Target"] = caplin_point["Close-Open"].apply(lambda x: 1 if x>0 else 0).shift(-1)
 
 joblib.dump(caplin_point, "CAPLIN_POINT.joblib")
