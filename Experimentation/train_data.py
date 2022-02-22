3ed31493c195ac87d56aaea268a685f6f2814a97
hithesh111 <hithesh111@gmail.com>
2022-02-22 19:46:58 +0530

    Added lags of 30 days for Volatility, Percent_Change, Target and Close-Open

--- Experimentation/train_data.py
+++ Experimentation/train_data.py
@@ -8,45 +8,27 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.metrics import accuracy_score
 
-data = joblib.load("Experimentation/CAPLIN_POINT.joblib")
-X = data.drop(["Target"],axis=1).iloc[4:-1,:]
-y = data["Target"].iloc[4:-1]
+data = joblib.load("Experimentation/DABUR.joblib")
+X = data.drop(["Target"],axis=1).iloc[30:-1,:]
+y = data["Target"].iloc[30:-1]
 
 split = int(X.shape[0]*0.8)
-test_size = 100
+# test_size = 100
 X_train, X_test, y_train, y_test = X.iloc[:split,:], X.iloc[split:,:], y.iloc[:split], y.iloc[split:]
 # X_train, X_test, y_train, y_test = X.iloc[:-test_size,:], X.iloc[-test_size:,:], y.iloc[:-test_size], y.iloc[-test_size:]
 
-attributes = ["Volatility", "Percent_Change", "Open"]
+attributes = X.columns
 
 #Predict Majority Class
 majority = np.zeros((X.shape[0])) + int(y.mean())
 score = accuracy_score(y,majority)
 print("\nMajority Prediction:",score)
 
-KNN = KNeighborsClassifier(30)
-KNN.fit(X_train[attributes],y_train)
-pred = KNN.predict(X_test[attributes])
-score = accuracy_score(y_test,pred)
-print("\nKNN Score:",score)
-
-LR = LogisticRegression()
+LR = LogisticRegression(penalty = "l1", solver = "liblinear")
 LR.fit(X_train[attributes],y_train)
 pred = LR.predict(X_test[attributes])
+pred_train = LR.predict(X_train[attributes])
+train_score = accuracy_score(y_train,pred_train)
 test_score = accuracy_score(y_test,pred)
+print("\nLogistic Regression Train Score:",train_score)
 print("\nLogistic Regression Test Score:",test_score)
-
-SVM = SVC()
-SVM.fit(X_train[attributes],y_train)
-pred = SVM.predict(X_test[attributes])
-score = accuracy_score(y_test,pred)
-print("\nSVM Score:",score)
-
-GNB = GaussianNB(priors=[1-y.mean(), y.mean()])
-GNB.fit(X_train[attributes],y_train)
-pred = GNB.predict(X_test[attributes])
-score = accuracy_score(y_test,pred)
-print("\nGNB Score:",score)
-
-
-print("\n")
