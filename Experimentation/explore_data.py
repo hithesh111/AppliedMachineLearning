f282d1c48ec3e57dd87c25123671b8767d5a7a89
hithesh111 <hithesh111@gmail.com>
2022-02-23 21:31:24 +0530

    Added Moving Averages and Moving Bounds

--- Experimentation/explore_data.py
+++ Experimentation/explore_data.py
@@ -2,6 +2,9 @@ import joblib
 import matplotlib.pyplot as plt
 data = joblib.load("Experimentation/data.joblib")
 
-plt.plot(data["Close"])
+data[["Close","SMA50","SMA100","SMA200"]].plot()
+plt.savefig('Experimentation/plots/MovingAverages.png')
 
-plt.savefig('Experimentation/plots/ClosePrices.png')
+plt.close()
+data[["Close","SMM50","SMM100","SMM200","SMm50","SMm100","SMm200"]].plot()
+plt.savefig('Experimentation/plots/MovingMaximum.png')
--- Experimentation/prepare_data.py
+++ Experimentation/prepare_data.py
@@ -1,17 +1,30 @@
 import yfinance as yf
 import joblib
 
-data = yf.download("ITC.NS", start = "2012-01-01", end = "2022-02-03")
+data = yf.download("KSCL.BO")
 
 data["Close-Open"] = data["Close"] - data["Open"]
 data["Percent_Change"] = 100*data["Close-Open"]/data["Open"]
 data["Volatility"] = 100*(data["High"] - data["Low"])/data["Open"] 
 data["Target"] = data["Close-Open"].apply(lambda x: 1 if x>0 else 0).shift(-1)
-data["SupRes1"] = data["Close"].apply(lambda x: 1 if x>260 else 0)
-data["SupRes2"] = data["Close"].apply(lambda x: 1 if x>230 else 0)
-data["SupRes3"] = data["Close"].apply(lambda x: 1 if x>200 else 0)
+data["SupRes1"] = data["Close"].apply(lambda x: 1 if x>610 else 0)
+data["SupRes2"] = data["Close"].apply(lambda x: 1 if x>500 else 0)
+data["SupRes3"] = data["Close"].apply(lambda x: 1 if x>300 else 0)
 
-n_days = 2
+data['SMA50'] = data['Close'].rolling(50).mean()
+data['SMA100'] = data['Close'].rolling(100).mean()
+data['SMA200'] = data['Close'].rolling(200).mean()
+
+data['SMM50'] = data['Close'].rolling(50).max()
+data['SMM100'] = data['Close'].rolling(100).max()
+data['SMM200'] = data['Close'].rolling(200).max()
+
+data['SMm50'] = data['Close'].rolling(50).min()
+data['SMm100'] = data['Close'].rolling(100).min()
+data['SMm200'] = data['Close'].rolling(200).min()
+
+
+n_days = 4
 
 for i in range(1,n_days+1):
     data["PrevTarget_"+str(i)+"lag"] = data["Target"].shift(i)
