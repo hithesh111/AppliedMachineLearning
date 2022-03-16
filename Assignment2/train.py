a60b6b155c84e37d179804b0d8108e4fc656d730
hithesh111 <hithesh111@gmail.com>
2022-03-16 20:32:32 +0530

    Added Documentation and Run FIle

--- Assignment2/train.py
+++ Assignment2/train.py
@@ -1,5 +1,3 @@
-from msilib.schema import Class
-from re import X
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import LSTM
@@ -14,12 +12,14 @@ import logging
 
 class Training():
     def DataSplit(self,X,y):
+        #Do a split of training and testing data
         split = int(0.8*X.shape[0])
         X_train, X_test = X[:split], X.iloc[split:]
         y_train, y_test = y[:split], y[split:]
         return X_train, X_test, y_train, y_test
 
     def Fit_Scaler(self,X_train,X_test,y_train,y_test, scaler):
+        #Fit a scaler to training attributes and then scale the rest using same scaler
         X_train=scaler.fit_transform(np.array(X_train)).reshape(-1,1)
         X_test=scaler.transform(np.array(X_test)).reshape(-1,1)
         y_train=scaler.transform(np.array(y_train).reshape(-1,1))
@@ -27,6 +27,7 @@ class Training():
         return X_train, X_test, y_train, y_test
 
     def create_model(self,input_length):
+        #Create an empty LSTM model
         model=Sequential()
         model.add(LSTM(100,return_sequences=True,input_shape=(input_length,1)))
         model.add(LSTM(100, return_sequences = True))
@@ -36,12 +37,14 @@ class Training():
         return model
 
     def train_validate_predict(self,X_train,X_test,y_train,y_test, model):
+        #Fit the model using training data and make predictions for cross validation
         model.fit(X_train, y_train, epochs=50,validation_data=(X_test,y_test), verbose=1)
         train_predict=model.predict(X_train)
         test_predict=model.predict(X_test)
         return model, train_predict, test_predict
 
     def ScalerInvert(self,scaler, X_train,X_test, y_train, y_test, train_predict, test_predict):
+        #Invert the training/testing data as well as the predictions
         train_predict=scaler.inverse_transform(train_predict)
         test_predict=scaler.inverse_transform(test_predict)
         X_train=scaler.inverse_transform(X_train)
@@ -51,9 +54,11 @@ class Training():
         return X_train, X_test, y_train, y_test, train_predict, test_predict
 
     def CrossValScore(self,train_predict, y_train, test_predict, y_test):
-            return r2_score(train_predict,y_train), r2_score(test_predict,y_test)
+        #Get r2 score of the regression prediction
+        return r2_score(train_predict,y_train), r2_score(test_predict,y_test)
 
     def CombineResults(self,data,X_train, X_test, y_train, y_test, train_predict, test_predict):
+        #Combine the predictions to the dataframe
         y_rec = np.concatenate([y_train,y_test],axis = 0)
         y_pred = np.concatenate([train_predict , test_predict], axis= 0)
         X_rec = np.concatenate([X_train,X_test],axis = 0)
--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 Repository for Applied Machine Learning Coursework (Jan 2022 - Mar 2022)
 
-<a href = "https://github.com/hithesh111/AppliedMachineLearning/tree/main/Assignment%201">Assignment 1</a>
-<a href = "https://github.com/hithesh111/AppliedMachineLearning/tree/main/Assignment%202">Assignment 2</a>
+<a href = "https://github.com/hithesh111/AppliedMachineLearning/tree/main/Assignment1">Assignment 1</a>
+
+<a href = "https://github.com/hithesh111/AppliedMachineLearning/tree/main/Assignment2">Assignment 2</a>
