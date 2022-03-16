a6fb4df8f41a457affce436ea8f1c35a317390f9
hithesh111 <hithesh111@gmail.com>
2022-03-16 10:48:39 +0530

    Object Oriented

--- CI/prepare_data.py
+++ CI/prepare_data.py
@@ -1,13 +1,33 @@
-from calendar import weekday
-from sys import prefix
 import yfinance as yf
 import joblib
 import pandas as pd
-from datetime import datetime
 
-data = yf.download("ITC.NS",start = "2020-01-01", end = "2021-12-31")
-# data["Target"] = (data["Close"] - data["Open"]).apply(lambda x: 1 if x>0 else 0).shift(-1)
-# data = data[["Close","Open","Target"]]
-data = data[["Close"]]
-data["Target"] = data["Close"].shift(-1)
-joblib.dump(data, "joblib/data.joblib")
+stock_ticker = "ITC.NS"
+start_date = "2020-01-01"
+end_date = "2021-12-31"
+data_path = "joblib/data.joblib"
+
+class Prepare():
+    def load(self, stock_ticker, start_date, end_date):
+        data = yf.download(stock_ticker, start_date, end_date)
+        return data
+    
+    def preprocess(self,data):
+        data = data[["Close"]]
+        data["Target"] = data["Close"].shift(-1)
+        return data
+        
+    def data_dump(self, data, data_path):
+        joblib.dump(data, data_path)
+
+    def __init__(self, stock_ticker, start_date, end_date, data_path):
+        data = yf.download(stock_ticker, start_date, end_date)
+        data = self.preprocess(data)
+        self.data_dump(data, data_path)
+
+if __name__ == "__main__":
+    stock_ticker = "ITC.NS"
+    start_date = "2020-01-01"
+    end_date = "2021-12-31"
+    data_path = "joblib/data.joblib"
+    Prepare(stock_ticker, start_date, end_date, data_path)
